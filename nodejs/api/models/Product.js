a1dcb462a47c56668993eec4b23949e63f565569
farahmed97 <farah_ahmed1997@yahoo.com>
2018-03-02 22:44:32 +0200

    added 2 folders

---
+++ nodejs/api/models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
